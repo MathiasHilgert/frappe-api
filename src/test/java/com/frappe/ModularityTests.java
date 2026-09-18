6c95c42dfa1dac408c1910daef8cd1409b52d772
Mathias Hilgert <matthiashilgertlol@gmail.com>
2026-09-18 18:41:13 -0300

    test: verify module boundaries with spring modulith

---
+++ src/test/java/com/frappe/ModularityTests.java
@@ -0,0 +1,21 @@
+package com.frappe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
+
+class ModularityTests {
+
+	ApplicationModules modules = ApplicationModules.of(FrappeApiApplication.class);
+
+	@Test
+	void verifiesModularStructure() {
+		modules.verify();
+	}
+
+	@Test
+	void writesDocumentation() {
+		new Documenter(modules).writeDocumentation();
+	}
+
+}
